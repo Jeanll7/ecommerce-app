ad7a293b1efdd211088011992507744ce2295460
Jeanll7 <jeancarloleall@gmail.com>
2024-04-29 16:17:59 -0300

    feat:component product

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,7 @@ const Product = ({ product }) => {
 
   return (
     <div>
-      <div className='border border-[#e4e4e4] h-[300px] mb-4 overflow-hidden group transition'>
+      <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
         <div className='w-full h-full flex justify-center items-center'>
           {/* image */}
           <div className='w-[200px] mx-auto flex justify-center items-center'>
@@ -17,6 +17,16 @@ const Product = ({ product }) => {
               src={image} alt='' 
             />
           </div>
+        </div>
+        <div className='absolute top-0 right-0 bg-red-500/40 p-2'>
+          <button>
+            <div className='flex justify-center items-center text-white w-12 h-12'>
+              <BsPlus className='text-3xl' />
+            </div>
+          </button>
+          <Link to={'/'} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'>
+            <BsEyeFill />
+          </Link>
         </div>
       </div>        
       <div>2</div>      
