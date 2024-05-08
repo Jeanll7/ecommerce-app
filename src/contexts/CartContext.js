fedc08d9dc9caeffafa35e5a794f91bd176a9e93
Jeanll7 <jeancarloleall@gmail.com>
2024-05-08 00:53:52 -0300

    feat: add item the cart

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,23 +1,38 @@
-import React, {createContext, useState, useEffect} from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
 //create context
-export const CartContext = createContext()
+export const CartContext = createContext();
 
-const CartProvider = ({children}) => {
+const CartProvider = ({ children }) => {
   // cart state
-  const [cart, setCart] = useState([])
-  // add to cart 
+  const [cart, setCart] = useState([]);
+  // add to cart
   const addToCart = (product, id) => {
-    console.log(product)
-    console.log(`${product.title} added to the cart`)
+    const newItem = { ...product, amount: 1 };
+    // verificar se o item já está no carrinho
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
   }
+  console.log(cart)
 
   return (
     <CartContext.Provider value={{ addToCart }}>
       {children}
     </CartContext.Provider>
-  )
-  
+  );
 };
 
 export default CartProvider;
