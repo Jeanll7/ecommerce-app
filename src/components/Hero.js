436985064ae0a9c1f18efc28dbc2e03c259d8499
Jeanll7 <jeancarloleall@gmail.com>
2024-05-13 16:13:10 -0300

    feat: add hero image page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import WomanImg from '../img/woman_hero.png'
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
+      <div className='container mx-auto flex justify-around h-full'>
+        {/* texto */}
+        <div className='flex'>
+          <div>
+            <div>Nova tendência</div>
+          </div>
+        </div>
+        {/* image */}
+        <div className='hidden lg:block'>
+          <img src={WomanImg} alt='' />
+        </div>
+      </div>
+
+    </section>
+  )
 };
 
 export default Hero;
