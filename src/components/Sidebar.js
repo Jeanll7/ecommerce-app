9a2105c36aff0886aef750181918d7145c3ea4ca
Jeanll7 <jeancarloleall@gmail.com>
2024-05-13 17:18:53 -0300

    feat: add link view cart and checkout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,49 +1,71 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 // import link e icons
 // import { Link } from 'react-router-dom';
-import { IoMdArrowForward } from 'react-icons/io'
-import { FiTrash2 } from 'react-icons/fi'
-// import components 
-import CartItem from '../components/CartItem'
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+// import components
+import CartItem from "../components/CartItem";
 // import sidebar context
-import { SidebarContext } from '../contexts/SidebarContext';
+import { SidebarContext } from "../contexts/SidebarContext";
 // import cart context
-import { CartContext } from '../contexts/CartContext';
+import { CartContext } from "../contexts/CartContext";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  const { isOpen, handleClose } = useContext(SidebarContext)
-  const { cart, clearCart, total } = useContext(CartContext)
-  
+  const { isOpen, handleClose } = useContext(SidebarContext);
+  const { cart, clearCart, total } = useContext(CartContext);
+
   return (
-    <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+    <div
+      className={`${
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
-      <div className='flex items-center justify-between py-6 border-b'>
-        <div className='uppercase text-sm font-semibold'>Shopping Bag (0)</div>
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
         {/* icon  */}
-        <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
+        <div
+          onClick={handleClose}
+          className="cursor-pointer w-8 h-8 flex justify-center items-center"
+        >
           <IoMdArrowForward size={24} />
         </div>
       </div>
 
-      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>
+      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
         {cart.map((item) => {
-          return <CartItem item={item} key={item.id} />
+          return <CartItem item={item} key={item.id} />;
         })}
       </div>
-      <div className='flex flex-col gap-y-3 py-4'>
-        <div className='flex w-full justify-between items-center'>
-          {/* total */}          
-          <div className='uppercase font-semibold'>
-            <span className='mr-2'>Total:</span>${parseFloat(total).toFixed(2)}
+      <div className="flex flex-col gap-y-3 py-4">
+        <div className="flex w-full justify-between items-center">
+          {/* total */}
+          <div className="uppercase font-semibold">
+            <span className="mr-2">Total:</span>${parseFloat(total).toFixed(2)}
           </div>
           {/* clear cart */}
-          <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center rounded-sm'>
+          <div
+            onClick={clearCart}
+            className="cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center rounded-sm"
+          >
             <FiTrash2 size={20} />
           </div>
         </div>
+        <Link
+          to="/"
+          className="bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium"
+        >
+          Ver carrinho
+        </Link>
+        <Link
+          to="/"
+          className="bg-primary flex p-4 justify-center items-center text-white w-full font-medium"
+        >
+          Finalizar
+        </Link>
       </div>
     </div>
-  )
+  );
 };
 
 export default Sidebar;
