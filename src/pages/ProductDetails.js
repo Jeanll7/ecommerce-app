9f16788487a3b152797e660b9934a6407ac139d6
Jeanll7 <jeancarloleall@gmail.com>
2024-05-22 09:28:24 -0300

    feat: add new button

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -32,13 +32,23 @@ const ProductDetails = () => {
         <div className="flex flex-col lg:flex-row items-center">
           {/* image */}
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
-            <img className="max-w-[200px] lg:max-w-xs" src={image} alt="" />
+            <img className="max-w-[220px] lg:max-w-sm" src={image} alt="" />
           </div>
           {/* text */}
           <div className="flex-1 text-center lg:text-left">
-            <h1 className="text-[24px] font-medium mb-2 max-w-[450px] mx-auto">{title}</h1>
-            <div className="text-xl text-red-500 font-medium mb=6">$ {price}</div>
-            <p>{description}</p>
+            <h1 className="text-[24px] font-medium mb-2 max-w-[450px] mx-auto lg:m-0">
+              {title}
+            </h1>
+            <div className="text-xl text-red-500 font-medium mb=6">
+              $ {price}
+            </div>
+            <p className="mb-8">{description}</p>
+            <button
+              onClick={() => addToCart(product, product.id)}
+              className="bg-primary text-white px-4 py-3 rounded"
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
